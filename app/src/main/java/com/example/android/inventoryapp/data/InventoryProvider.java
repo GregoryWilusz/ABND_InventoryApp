8506a887af20799a250e87baa8e6e0451db4d841
Greg <wilusz.gregory@gmail.com>
2018-06-07 20:57:36 +0200

    Added InventoryProvider boilerplate, registered it in AndroidManifest, added URI constants in InventoryContract

---
+++ app/src/main/java/com/example/android/inventoryapp/data/InventoryProvider.java
@@ -0,0 +1,74 @@
+package com.example.android.inventoryapp.data;
+
+import com.example.android.inventoryapp.data.InventoryContract.ProductEntry;
+
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.content.UriMatcher;
+import android.database.Cursor;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by grzegorzwilusz on 6/7/18.
+ */
+
+public class InventoryProvider extends ContentProvider {
+
+    public static final String LOG_TAG = InventoryProvider.class.getName();
+
+    private InventoryDbHelper mInventoryDbHelper;
+
+    private static final int PRODUCTS = 100;
+    private static final int PRODUCT_ID = 101;
+    private static final UriMatcher sUriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+
+    static {
+        sUriMatcher.addURI(InventoryContract.CONTENT_AUTHORITY, InventoryContract.PATH_PRODUCTS, PRODUCTS);
+        sUriMatcher.addURI(InventoryContract.CONTENT_AUTHORITY, InventoryContract.PATH_PRODUCTS + "/#", PRODUCT_ID);
+    }
+
+    @Override
+    public boolean onCreate() {
+        mInventoryDbHelper = new InventoryDbHelper(getContext());
+        return true;
+    }
+
+    @Nullable
+    @Override
+    public Cursor query(@NonNull Uri uri, @Nullable String[] projection, @Nullable String selection, @Nullable String[] selectionArgs, @Nullable String sortOrder) {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public Uri insert(@NonNull Uri uri, @Nullable ContentValues values) {
+        return null;
+    }
+
+    @Override
+    public int delete(@NonNull Uri uri, @Nullable String selection, @Nullable String[] selectionArgs) {
+        return 0;
+    }
+
+    @Override
+    public int update(@NonNull Uri uri, @Nullable ContentValues values, @Nullable String selection, @Nullable String[] selectionArgs) {
+        return 0;
+    }
+
+    @Nullable
+    @Override
+    public String getType(@NonNull Uri uri) {
+        final int match = sUriMatcher.match(uri);
+
+        switch (match) {
+            case PRODUCTS:
+                return ProductEntry.CONTENT_LIST_TYPE;
+            case PRODUCT_ID:
+                return ProductEntry.CONTENT_ITEM_TYPE;
+            default:
+                    throw new IllegalArgumentException("Unknown URI " + uri + " with match " + match);
+        }
+    }
+}
